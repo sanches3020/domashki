98937c6b2794a7afbdb3a15a17ec20397e223877
sanches3020 <evmenovasofia7@gmail.com>
2025-02-14 09:51:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import sqlite3
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request,redirect, url_for, flash
+import os
 
 app = Flask(__name__)
+app.secret_key = '1234'
 
 @app.route('/add/')
 def add():
     return render_template('add.html')
 
-@app.route('/save_post/') 
+@app.route('/upload/', methods=['POST']) 
 def save_post():
     title = request.form['title']
-    file_name = request.form['file_name']
-    description = request.form['description']
-    
+    description = request.form['description']  
     image = request.files.get('image')
+    file_name = image.filename
+
     print(image.filename)
     print(image.name)
-    print(image.content_type)
-    image.save(f'D:\college\flask\static\uploads')
+    print(image.content_type)  
+    image.save(f'static/uploads/{file_name}')
 
-    conn = sqlite3.connect('database_name.db')
+    conn = sqlite3.connect('database_name.db') 
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO bd1 (title,file_name,description)
@@ -28,22 +30,17 @@ def save_post():
     conn.commit()
     conn.close()
 
-    return redirect (url_for("add.html"))
+    flash('Пост успешно сохранен!', 'success')
+    return redirect (url_for("add"))
 
 @app.route('/all_post/') 
 def all_post():
-    if request.method== 'POST':
-        title = request.form['title']
-        file_name = request.form['file_name']
-        description = request.form['description']
-
     conn = sqlite3.connect('database_name.db')
     cursor = conn.cursor()
-    cursor.execute("""
-                        SELECT * FROM bd1 WHERE title= ? AND file_name=? AND description=?""",[title,file_name,description])
-    if cursor.fetchone():
-       conn.close()
-    return render_template('index.html')
+    cursor.execute("SELECT * FROM bd1")
+    posts = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
